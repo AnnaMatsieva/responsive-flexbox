fc47dde49ecf655341d8c832c688f23ae8ecc7d4
AnnaMatsieva <ann.matsieva@gmail.com>
2017-10-19 23:42:06 +0300

    tablet rework

--- js/user.js
+++ js/user.js
@@ -5,3 +5,24 @@ $(document).on('ready', function() {
         infinite: true
     });
 });
+
+/*just a test of work "load more button"*/
+$(document).ready(function () {
+    
+    $("div").each(function (index) {
+        $(this).find('.b-model li:lt(' + $(this).attr('viewChild') + ')').show();
+    });   
+
+    $('.loadMore').click(function () {
+        var $myWrapper= $(this).closest('div');
+        var x= parseInt($myWrapper.attr('viewChild'),10);
+        var liSize=$myWrapper.find('.b-model li').size();        
+        x = (x + 5 <= liSize) ? x + 5 : liSize;
+        $myWrapper.attr('viewChild',x)
+        $myWrapper.find('.b-model li:lt(' + x + ')').show();
+        $myWrapper.find('.showLess').show();
+        if (x == liSize) {
+            $myWrapper.find('.loadMore').hide();
+        }
+    });
+});
